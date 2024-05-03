f42f998fb320c2596d9c952f6a996a53dff707f3
giant <giant.wang@dotdot.cc>
2024-05-03 11:45:10 +0800

    practice svatle

---
+++ src/routes/todolist/+page.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+	<title>Todo list</title>
+	<meta name="description" content="Todo list" />
+</svelte:head>
+
+<script>
+	import Todos from './components/Todos.svelte'
+	import './styles.css';
+
+
+	let todos = [
+		{ id: 1, name: 'Create a Svelte starter app', completed: true },
+		{ id: 2, name: 'Create your first component', completed: true },
+		{ id: 3, name: 'Complete the rest of the tutorial', completed: false }
+	]
+</script>
+
+<div class="text-column">
+	<Todos {todos} />
+</div>
---
+++ src/routes/todolist/components/Todos.svelte
@@ -0,0 +1,91 @@
+<script>
+  import FilterButton from './FilterButton.svelte'
+  import Todo from './Todo.svelte'
+  import MoreActions from "./MoreActions.svelte";
+  import { onMount } from "svelte";
+
+  export let todos = []
+  let nameEl;
+  
+  let newTodoName = ''
+  $: newTodoId = totalTodos ? Math.max(...todos.map(t => t.id)) + 1 : 1
+
+  $: totalTodos = todos.length
+  $: completedTodos = todos.filter(todo => todo.completed).length
+
+  onMount(() => {
+    nameEl.focus()
+  });
+
+  const removeTodo = (todo) => {
+    todos = todos.filter(t => t.id !== todo.id)
+  }
+
+  const addTodo = () => {
+    todos = [...todos, { id: newTodoId, name: newTodoName, completed: false }]
+    newTodoName = ''
+  }
+
+  const updateTodo = (todo) => {
+    const i = todos.findIndex(t => t.id === todo.id)
+    todos[i] = { ...todos[i], ...todo }
+  }
+
+  let filter = 'all'
+  const filterTodos = (filter, todos) => 
+    filter === 'active' ? todos.filter(t => !t.completed) :
+    filter === 'completed' ? todos.filter(t => t.completed) : 
+    todos
+
+  const checkAllTodos = (completed) => {
+    todos = todos.map((t) => ({ ...t, completed }));
+  };
+
+  const removeCompletedTodos = () =>
+    (todos = todos.filter((t) => !t.completed));
+</script>
+
+<!-- Todos.svelte -->
+<div class="todoapp stack-large">
+
+  <!-- NewTodo -->
+  <form on:submit|preventDefault={addTodo}>
+    <h2 class="label-wrapper">
+      <label for="todo-0" class="label__lg">
+        What needs to be done?
+      </label>
+    </h2>
+    <input bind:value={newTodoName} type="text" bind:this={nameEl} id="todo-0" autocomplete="off" class="input input__lg" />
+    <button type="submit" disabled="" class="btn btn__primary btn__lg">
+      Add
+    </button>
+  </form>
+
+  <!-- Filter -->
+  <FilterButton bind:filter />
+
+  <!-- TodosStatus -->
+  <h2 id="list-heading">{completedTodos} out of {totalTodos} items completed</h2>
+
+  <!-- Todos -->
+  <ul role="list" class="todo-list stack-large" aria-labelledby="list-heading">
+  {#each filterTodos(filter, todos) as todo (todo.id)}
+    <li class="todo">
+      <Todo {todo}
+        on:update={e => updateTodo(e.detail)}
+        on:remove={e => removeTodo(e.detail)}
+      />
+    </li>
+  {:else}
+    <li>Nothing to do here!</li>
+  {/each}
+  </ul>
+
+  <hr />
+
+  <!-- MoreActions -->
+  <MoreActions {todos}
+    on:checkAll={(e) => checkAllTodos(e.detail)}
+    on:removeCompleted={removeCompletedTodos}
+  />
+</div>
