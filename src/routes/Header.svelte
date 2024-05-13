2a03dae33485225353b49ecf57df361b1ecb7a9b
giant <giant.wang@dotdot.cc>
2024-05-13 16:53:57 +0800

    pages

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,6 +2,7 @@
 	import { page } from '$app/stores';
 	import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
+	import { base } from '$app/paths';
 </script>
 
 <header>
@@ -17,19 +18,19 @@
 		</svg>
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
+				<a href="{base}/">Home</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+				<a href="{base}/about">About</a>
 			</li>
 			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
+				<a href="{base}/sverdle">Sverdle</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/todolist' ? 'page' : undefined}>
-				<a href="/todolist">Todo list</a>
+				<a href="{base}/todolist">Todo list</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/dnd' ? 'page' : undefined}>
-				<a href="/dnd">DND</a>
+				<a href="{base}/dnd">DND</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
